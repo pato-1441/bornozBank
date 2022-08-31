279079c878a2ab1982aa36f7b8e5fc20ce928ea1
Patricio Albornoz <wagnerpatricio@outlook.com.ar>
2022-08-31 10:44:52 -0300

    working on contacts

--- src/js/contactos.js
+++ src/js/contactos.js
@@ -27,7 +27,6 @@ const inputCBUAddContact = document.getElementById('inputCBUAddContact');
 const inputAliasAddContact = document.getElementById('inputAliasAddContact');
 const submitAddContact = document.getElementById('submitAddContact');
 const contenedorContactos = document.getElementById('contenedorContactos');
-const template = document.getElementById('template').content;
 const fragment = document.createDocumentFragment();
 let contacts = JSON.parse(localStorage.getItem('contactos'));
 
@@ -56,16 +55,105 @@ const crearContacto = e => {
 }
 
 const pintarContactos = () =>{
+    let contador = 0;
     contenedorContactos.innerHTML='';
     contacts.forEach(contacto => {
-        const cloneTemplate = template.cloneNode(true);
-        cloneTemplate.getElementById('contactoFullname').textContent = contacto.contactFullname;
-        fragment.appendChild(cloneTemplate);
+        contador++
+        contenedorContactos.innerHTML+=`<label for="my-modal-${contador}" class="btn modal-button font-medium text-white">
+                                            <span class="ml-1" id="contactoFullname">${contacto.contactFullname}</span>
+                                        </label>
+                                        <input type="checkbox" id="my-modal-${contador}" class="modal-toggle" />
+                                        <!--cuerpo del modal-->
+                                        <label for="my-modal-${contador}" class="modal cursor-pointer">
+                                            <label
+                                                class="modal-box relative w-11/12 sm:w-8/12 h-fit max-w-5xl bg-white text-black flex flex-col"
+                                                for="">
+                                                <h2 class="text-black text-center text-2xl pt-4 w-2/3 font-semibold mx-auto">Editar contacto</h2>
+                                                <div class="form flex flex-col items-center">
+                                                    <!--arranca añadir contacto-->
+                                                    <div class="form-control w-full max-w-xs mx-auto pt-4 pb-4" id="editContactForm">
+                                                        <!--arranca nombre contacto-->
+                                                        <label class="label">
+                                                            <span class="label-text text-base text-black">Nombre Completo</span>
+                                                        </label>                                    
+                                                        <input required type="text" placeholder="Nombre completo"
+                                                                class="input input-bordered w-full max-w-xs bg-white" minlength="4"
+                                                                maxlength="30" id="inputNameEditContact" value="${contacto.contactFullname}" />
+                                                        <label class="label">
+                                                            <span class="label-text-alt text-sm" id="previousName">Nombre previo: ${contacto.contactFullname}</span>                                       
+                                                        </label>
+                                                        <!--termina nombre contacto-->
+                                                        <!--arranca cbu contacto-->
+                                                        <label class="label">
+                                                            <span class="label-text text-base text-black">CBU</span>
+                                                        </label>
+                                                        <input required type="text" placeholder="CBU"
+                                                            class="input input-bordered w-full max-w-xs bg-white" minlength="22"
+                                                            maxlength="22" id="inputCBUEditContact" value="${contacto.contactCBU}"/>
+                                                        <label class="label">
+                                                            <span class="label-text-alt text-sm">CBU previo: ${contacto.contactCBU}</span>                                       
+                                                        </label>
+                                                        <!--termina cbu contacto-->
+                                                        <!--arranca alias contacto-->
+                                                        <label class="label">
+                                                            <span class="label-text text-base text-black">Alias</span>
+                                                        </label>
+                                                        <input required type="text" placeholder="Alias"
+                                                            class="input input-bordered w-full max-w-xs bg-white" minlength="2"
+                                                            maxlength="40" id="inputAliasEditContact" value="${contacto.contactAlias}"/>
+                                                        <label class="label">
+                                                            <span class="label-text-alt text-sm">Alias previo: ${contacto.contactAlias}</span>                                       
+                                                        </label>
+                                                        <!--termina alias contacto-->
+                                                        <!--boton añadir contacto-->
+                                                        <div class="mt-4 flex mx-auto">
+                                                            <button type="submit" id="submitEditContact"
+                                                                class="btn btn-outline w-1/2 mr-1 text-white hover:text-blue-500 hover:border-blue-500 hover:bg-inherit bg-blue-500 mb-3">Guardar cambios
+                                                            </button>
+                                                            <button type="submit" id="submitDeleteContact"
+                                                                class="btn btn-outline w-1/2 text-white hover:text-red-600 hover:border-red-600 hover:bg-inherit bg-red-600 mb-3">Eliminar
+                                                                contacto
+                                                            </button>
+                                                        </div>
+                                                        <!--termina añadir contacto-->
+                                                        <!--arranca alerta contacto añadido con exito-->
+                                                        <div class="alert alert-success bg-green-400 text-white hidden"
+                                                            id="alertEditContactSuccess">
+                                                            <div><svg xmlns="http://www.w3.org/2000/svg"
+                                                                    class="stroke-current flex-shrink-0 h-6 w-6" fill="none"
+                                                                    viewBox="0 0 24 24">
+                                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                                        d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+                                                                <span>Se ha modificado correctamente el contacto.</span>
+                                                            </div>
+                                                        </div>
+                                                        <!--termina alerta contacto añadido con exito-->
+                                                        <!--arranca error en añadir contacto-->
+                                                        <div class="alert alert-error text-white bg-red-700 hidden" id="alertEditContactError">
+                                                            <div>
+                                                                <svg xmlns="http://www.w3.org/2000/svg"
+                                                                    class="stroke-current flex-shrink-0 h-6 w-6" fill="none"
+                                                                    viewBox="0 0 24 24">
+                                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                                        d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                                                </svg>
+                                                                <span>Error! Complete todos los campos por favor.</span>
+                                                            </div>
+                                                        </div>
+                                                        <!--termina error en añadir contacto-->
+                                                    </div>
+                                                    <!--termina form interno-->
+                                                </div>
+                                                <!--termina form contacto-->
+                                            </label>
+                                        </label>
+                                        <!--termina modal añadir contactos-->`
+        //fragment.appendChild();
     });
     contenedorContactos.appendChild(fragment);
 }
 
-pintarContactos();
+addEventListener('DOMContentLoaded',()=>{pintarContactos()})
 
 /*
 const cargarContactos=()=>{
